c8f89358488ce2f6a53b1b70bb535e536262215a
Merve Erdol <merve-uygun@windowslive.com>
2017-10-26 16:30:38 +0300

    Add starter files

---
+++ index.js
@@ -0,0 +1,16 @@
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
